f4fe8ff48c8a8a0ffe448a473001925da538a420
moad6127 <moad6127@gmail.com>
2022-05-13 15:09:45 +0900

    Feat : random
    셔플하기

--- check/main.cpp
+++ check/main.cpp
@@ -1,31 +1,18 @@
 #include<iostream>
 #include<random>
+#include<vector>
 int main()
 {
-
-	//C style
-		//srand((unsigned int)time(nullptr));
-		//for (int i = 0; i < 20; i++)
-		//{
-		//	std::cout << rand() << " ";
-		//}
-
-		//std::cout << std::endl;
-	//C++ style
 	std::random_device rd;
-	std::mt19937 gen(rd()); //functor : function + Object
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<> dis(0,9);
 
-	for (int i = 0; i < 20; i++)
+	std::vector<int> deck{ 1,2,3,4,5,6,7,8,9,10 };
+	
+	std::shuffle(deck.begin(), deck.end(),gen);
+	for (auto& e : deck)
 	{
-		std::cout << (gen()%6) + 1  << " "; //1~6까지의 수만 나오게 한다.
-
+		std::cout << e << " ";
 	}
-	std::cout << std::endl;
 
-	std::uniform_int_distribution<> dist(1, 6); //분포를 균등하게 한다.
-	for (int i = 0; i < 20; i++)
-	{
-		std::cout << dist(gen)<<" ";
-	}
-	std::cout << std::endl;
 }
